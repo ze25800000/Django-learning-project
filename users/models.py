f27ebc14a69001594d332f4ae5c2433fff1b28b2
ze25800000 <ze258100000@sina.com>
2018-04-05 07:35:17 +0800

    设计EmailVerifyRecord邮箱验证码 设计PageBanner轮播图

--- users/models.py
+++ users/models.py
@@ -1,4 +1,8 @@
 # _*_ encoding:utf-8 _*_
+# 系统的model
+from datetime import datetime
+
+# Django的model
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
@@ -18,3 +22,26 @@ class UserProfile(AbstractUser):
 
     def __unicode__(self):
         return self.username
+
+
+class EmailVerifyRecord(models.Model):
+    code = models.CharField(max_length=20, verbose_name=u"验证码")
+    email = models.EmailField(max_length=50, verbose_name=u"验证码")
+    send_type = models.CharField(choices=(('register', "注册"), ('forget', u"找回密码")), max_length=10, verbose_name=u"验证码")
+    send_time = models.DateTimeField(default=datetime.now)  # now必须把括号去掉
+
+    class Meta:
+        verbose_name = "邮箱验证码"
+        verbose_name_plural = verbose_name
+
+
+class Banner(models.Model):
+    title = models.CharField(max_length=100, verbose_name=u"标题")
+    image = models.ImageField(upload_to='banner/%Y/%m', verbose_name=u"轮播图", max_length=100)
+    url = models.URLField(max_length=200, verbose_name=u"访问地址")
+    index = models.IntegerField(default=100, verbose_name=u"顺序")
+    add_time = models.DateTimeField(default=datetime.now, verbose_name=u"添加时间")
+
+    class Meta:
+        verbose_name = "轮播图"
+        verbose_name_plural = verbose_name
