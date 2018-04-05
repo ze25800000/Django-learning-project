83dbd80064da5b5d83816eda1d399dc735cb2ec3
ze25800000 <ze258100000@sina.com>
2018-04-05 18:13:46 +0800

    用form验证传入数据合法性

--- apps/users/views.py
+++ apps/users/views.py
@@ -5,6 +5,7 @@ from django.db.models import Q  # 并集查询
 from django.views.generic.base import View
 
 from .models import UserProfile
+from .forms import LoginForm
 
 
 class CustomBackend(ModelBackend):
@@ -22,12 +23,15 @@ class LoginView(View):
         return render(request, "login.html", {})
 
     def post(self, request):
-        user_name = request.POST.get('username', '')
-        pass_word = request.POST.get('password', '')
-        user = authenticate(username=user_name, password=pass_word)
-        if user is not None:
-            login(request, user)
-            return render(request, 'index.html')
+        login_form = LoginForm(request.POST)
+        if login_form.is_valid():
+            user_name = request.POST.get('username', '')
+            pass_word = request.POST.get('password', '')
+            user = authenticate(username=user_name, password=pass_word)
+            if user is not None:
+                login(request, user)
+                return render(request, 'index.html')
+            else:
+                return render(request, 'login.html', {"msg": '用户名或密码错误'})
         else:
-            return render(request, 'login.html', {"msg": '用户名或密码错误'})
-
+            return render(request, 'login.html', {'login_form': login_form})
