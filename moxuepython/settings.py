83338a988744b3ad476e635667c9dabc11dd1aa0
ze25800000 <ze258100000@sina.com>
2018-04-05 09:32:10 +0800

    修复bug，不可以在MIDDLEWARE_CLASS中添加app文件

--- moxuepython/settings.py
+++ moxuepython/settings.py
@@ -11,9 +11,11 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+import sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
@@ -50,11 +52,7 @@ MIDDLEWARE_CLASSES = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'users',
-    'courses',
-    'operation',
-    'organization',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware'
 ]
 
 AUTH_USER_MODEL = "users.UserProfile"
