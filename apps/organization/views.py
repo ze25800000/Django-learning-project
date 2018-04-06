98353e96fc6edf8dea36ad53b13f3a6463b22c53
ze25800000 <ze258100000@sina.com>
2018-04-06 12:09:04 +0800

    列表筛选功能

--- apps/organization/views.py
+++ apps/organization/views.py
@@ -8,8 +8,26 @@ from .models import CourseOrg, CityDict
 class OrgListView(View):
     def get(self, request):
         all_orgs = CourseOrg.objects.all()
-        org_nums = all_orgs.count()
         all_cities = CityDict.objects.all()
+        # 授课机构排名
+        hot_orgs = all_orgs.order_by('click_nums')[:3]
+        # 筛选城市
+        city_id = request.GET.get('city', '')
+        if city_id:
+            all_orgs = all_orgs.filter(city_id=int(city_id))
+
+        # 类别筛选
+        category = request.GET.get('ct', '')
+        if category:
+            all_orgs = all_orgs.filter(category=category)
+        # 学习人数
+        sort = request.GET.get('sort', '')
+        if sort:
+            if sort == 'students':
+                all_orgs = all_orgs.order_by('-students')
+            elif sort == 'courses':
+                all_orgs = all_orgs.order_by('-students')
+
         try:
             page = request.GET.get('page', 1)
         except PageNotAnInteger:
@@ -19,8 +37,14 @@ class OrgListView(View):
 
         orgs = p.page(page)
 
+        org_nums = all_orgs.count()
+
         return render(request, 'org-list.html', {
             'all_orgs': orgs,
             'all_cities': all_cities,
-            'org_nums': org_nums
+            'org_nums': org_nums,
+            'city_id': city_id,
+            'category': category,
+            'hot_orgs': hot_orgs,
+            'sort': sort
         })
