26fec21cfc7e92304f4a81b943babd9b82e0303e
ze25800000 <ze258100000@sina.com>
2018-04-08 07:08:32 +0800

    视频播放功能

--- apps/courses/urls.py
+++ apps/courses/urls.py
@@ -3,7 +3,7 @@ __author__ = 'yangze'
 __date__ = '2018/4/7  5:26'
 
 from django.conf.urls import url
-from .views import CourseListView, CourseDetailView, CourseVideoView, CourseCommentView, AddCommentView
+from .views import CourseListView, CourseDetailView, CourseVideoView, CourseCommentView, AddCommentView,VideoPlayView
 
 urlpatterns = [
     url(r'^list/$', CourseListView.as_view(), name='course_list'),
@@ -12,5 +12,6 @@ urlpatterns = [
     url(r'^comment/(?P<course_id>\d+)/$', CourseCommentView.as_view(), name='course_comment'),
     # 添加课程评论
     url(r'^add_comment/$', AddCommentView.as_view(), name='add_comment'),
+    url(r'^video_play/(?P<video_id>\d+)/$', VideoPlayView.as_view(), name='video-play'),
 
 ]
--- apps/courses/views.py
+++ apps/courses/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views.generic import View
 from pure_pagination import Paginator, PageNotAnInteger
 from django.http import HttpResponse
-from .models import Course, CourseResource
+from .models import Course, CourseResource, Video
 from operation.models import UserFavorite, CourseComments, UserCourse
 from utils.mixin_urls import LoginRequiredMixin
 
@@ -118,3 +118,30 @@ class AddCommentView(View):
             return HttpResponse("{'status':'success','msg':'添加成功'}", content_type='application/json')
         else:
             return HttpResponse("{'status':'fail','msg':'添加失败'}", content_type='application/json')
+
+
+class VideoPlayView(View):
+    def get(self, request, video_id):
+        video = Video.objects.get(id=int(video_id))
+        course = video.lesson.course
+        course.students += 1
+        course.save()
+        # 查询用户是否已经关联了该课程
+        user_c = UserCourse.objects.filter(user=request.user, course=course)
+        if not user_c:
+            user_course = UserCourse(user=request.user, course=course)
+            user_course.save()
+
+        user_courses = UserCourse.objects.filter(course=course)
+        user_ids = [user_course.id for user_course in user_courses]
+        all_user_course = UserCourse.objects.filter(user_id__in=user_ids)
+        course_ids = [user_course.course.id for user_course in all_user_course]
+        # 获取学过该用户的其他所有课程
+        relate_courses = Course.objects.filter(id__in=course_ids).order_by('-click_nums')[:5]
+        all_resources = CourseResource.objects.filter(course=course)
+        return render(request, 'course-play.html', {
+            "course": course,
+            "all_resources": all_resources,
+            "relate_courses": relate_courses,
+            "video": video
+        })
